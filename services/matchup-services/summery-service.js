5c235b5199e31a655ea7420eb50406a5b7e7d74a
LumarisX <65790297+LumarisX@users.noreply.github.com>
2023-12-28 10:49:19 -0800

    add statistics to summery

--- services/matchup-services/summery-service.js
+++ services/matchup-services/summery-service.js
@@ -1,15 +1,18 @@
 const pokedexService = require('../pokedex-service.js')
 
-
 function summery(ateam, bteam) {
-  let aOut = []
+  let aOut = {}
+  aOut.team = []
   for (let m of ateam) {
-    aOut.push(summeryData(m.pid));
+    aOut.team.push(summeryData(m.pid));
   }
-  let bOut = []
+  aOut.stats = statistics(aOut.team)
+  let bOut = {}
+  bOut.team = []
   for (let m of bteam) {
-    bOut.push(summeryData(m.pid));
+    bOut.team.push(summeryData(m.pid));
   }
+  bOut.stats = statistics(bOut.team)
   return [aOut, bOut]
 }
 
@@ -21,6 +24,26 @@ function summeryData(pokemonData) {
   out.types = pokedexService.getTypes(pokemonData)
   out.baseStats = pokedexService.getBase(pokemonData)
   return out;
+}
+
+function statistics(team) {
+  let stats = {}
+  let all = { hp: [], atk: [], def: [], spa: [], spd: [], spe: [] }
+  for (let pokemon of team) {
+    for (let stat in pokemon.baseStats) {
+      all[stat].push(pokemon.baseStats[stat])
+    }
+  }
+  stats.mean = {}
+  stats.median = {}
+  stats.max = {}
+  for (let stat in all) {
+    all[stat].sort((a, b) => b - a)
+    stats.mean[stat] = Math.round(all[stat].reduce((x, y) => x + y) / team.length)
+    stats.median[stat] = all[stat][Math.round(all[stat].length / 2)]
+    stats.max[stat] = all[stat][0]
+  }
+  return stats
 }
 
 module.exports = { summery }
