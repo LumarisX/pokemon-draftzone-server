20e813d6aa41259d0097c54252417988dadeeaed
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-01-04 08:43:41 -0800

    matchup id overhaul

--- routes/draftRoute.js
+++ routes/draftRoute.js
@@ -1,62 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const UserModel = require("../models/userModel");
-const RoleModel = require("../models/roleModel")
+const MatchupModel = require("../models/matchupModel")
 const DraftModel = require("../models/draftModel");
 const Opponent = require("../classes/opponent")
 const Draft = require("../classes/draft")
+const ObjectId = require('mongoose').Types.ObjectId;
 
-router
-  .route('/')
-  .get(async (req, res) => {
-    try {
-      const users = await UserModel.find()
-      res.json(users);
-    } catch (error) {
-      res.status(500).json({ message: error.message })
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      if (req.body && req.body !== '') {
-        const newUser = new UserModel(req.body)
-        console.log(req.body)
-        await newUser.save()
-        return res.status(201).json({ message: "User created" })
-      } else {
-        return res.status(400).json({ message: error.message })
-      }
-
-    } catch (error) {
-      res.status(400).json({ message: error.message })
-    }
-  })
-
-router
-  .route('/:user_id')
-  .get((req, res) => {
-    try {
-      res.json(res.user);
-    } catch (error) {
-      res.status(500).json({ message: error.message })
-    }
-  })
-  .patch(async (req, res) => {
-    try {
-      await UserModel.findByIdAndUpdate(req.params.user_id, req.body)
-      res.send(`Update user with ID ${req.params.user_id}`)
-    } catch (error) {
-      res.status(400).json({ message: error.message })
-    }
-  })
-  .delete(async (req, res) => {
-    try {
-      await UserModel.findByIdAndDelete(req.params.user_id)
-      res.json({ message: 'Deleted User' })
-    } catch (error) {
-      res.status(500).json({ message: error.message })
-    }
-  });
 
 router.route("/:user_id/teams")
   .get(async (req, res) => {
@@ -103,11 +53,11 @@ router.route("/:user_id/:team_id")
       res.status(500).json({ message: error.message })
     }
   })
-  
-  router.route("/:user_id/:team_id/opponents")
+
+router.route("/:user_id/:team_id/opponents")
   .get(async (req, res) => {
     try {
-      res.json(await DraftModel.find({ owner: res.user.id }));
+      res.json(await MatchupModel.find({ 'aTeam._id': res.team._id }));
     } catch (error) {
       res.status(500).json({ message: error.message })
     }
@@ -126,7 +76,7 @@ router.route("/:user_id/:team_id")
     }
   })
 
-router.get("/:user_id/:team_id/:opponent_id", async (req, res) => {
+router.get("/:user_id/:team_id/:matchup_id", async (req, res) => {
   try {
     res.json(res.team.get("opponents")[req.params.opponent_id])
   } catch (error) {
@@ -152,16 +102,35 @@ router.param("team_id", async (req, res, next, team_id) => {
   let team;
   try {
     let user_id = await res.user.id;
-    team = await DraftModel.find({ owner: user_id, leagueId: team_id });
+    if (ObjectId.isValid(team_id)) {
+      team = await DraftModel.findById(team_id).lean();
+
+    } else {
+      team = await DraftModel.find({ owner: user_id, leagueId: team_id });
+      team = team[0]
+    }
     if (team == null) {
       return res.status(400).json({ message: 'Team id not found' })
     }
-    res.team = team[0];
+    res.team = team;
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
   next();
 });
 
+router.param("matchup_id", async (req, res, next, matchup_id) => {
+  let matchup;
+  try {
+    matchup = await MatchupModel.findById(matchup_id);
+    if (matchup_id == null) {
+      return res.status(400).json({ message: 'Team id not found' })
+    }
+    res.matchup = matchup
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  next();
+});
 
 module.exports = router;
