2b6579e00d7dbc8642c5d79dbd599ed6c4d3a52a
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-02-14 16:31:44 -0800

    report bug

--- services/draft-service.js
+++ services/draft-service.js
@@ -4,8 +4,8 @@ async function getScore(teamId) {
   let matchups = await getMatchups(teamId)
   let score = { wins: 0, loses: 0, diff: 0 }
   for (let matchup of matchups) {
-    if("score" in matchup.results)
-    let muScore = matchup.results.score
+    if("report" in matchup && "score" in matchup.report){
+    let muScore = matchup.report.score
     if (muScore[0] > muScore[1]) {
       score.wins++
       score.diff += muScore[0]-muScore[1]
@@ -14,11 +14,12 @@ async function getScore(teamId) {
       score.diff -= muScore[1]-muScore[0]
     }
   }
+  }
   return score
 }
 
 async function getMatchups(teamId) {
-  return await MatchupModel.find({ 'aTeam._id': teamId })
+  return await MatchupModel.find({ 'aTeam._id': teamId }).lean()
 }
 
 module.exports = { getScore }
