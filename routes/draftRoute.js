5a4a59b8206d920c695fd5c1b3cf357c789f5441
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-02-17 22:28:01 -0800

    fix draftRoute names

--- routes/draftRoute.js
+++ routes/draftRoute.js
@@ -35,8 +35,8 @@ router.route("/teams")
 router.route("/:team_id")
   .get(async (req, res) => {
     try {
-      res.team.score = await DraftService.getScore(res.team._id)
-      res.json(res.team)
+      res.draft.score = await DraftService.getScore(res.draft._id) 
+      res.json(res.draft)
     } catch (error) {
       res.status(500).json({ message: error.message })
     }
@@ -45,14 +45,14 @@ router.route("/:team_id")
 router.route("/:team_id/matchups")
   .get(async (req, res) => {
     try {
-      res.json(await MatchupModel.find({ 'aTeam._id': res.team._id }));
+      res.json(await MatchupModel.find({ 'aTeam._id': res.draft._id }));
     } catch (error) {
       res.status(500).json({ message: error.message })
     }
   })
   .post(async (req, res) => {
     try {
-      let matchup = new Matchup(req.body, res.team._id)
+      let matchup = new Matchup(req.body, res.draft._id)
       if (matchup.valid) {
         await matchup.model.save()
         res.status(201).json({ message: "Matchup Added" })
@@ -68,7 +68,7 @@ router.route("/:team_id/matchups")
 
 router.get("/:team_id/:matchup_id", async (req, res) => {
   try {
-    res.json(res.team.get("opponents")[req.params.opponent_id])
+    res.json(res.draft.get("opponents")[req.params.opponent_id])
   } catch (error) {
     req.status(500).json({ message: error.message })
   }
@@ -99,20 +99,21 @@ router.route("/test")
 // });
 
 router.param("team_id", async (req, res, next, team_id) => {
-  let team;
+  let draft;
   try {
     let user_id = await req.sub;
     if (ObjectId.isValid(team_id)) {
-      team = await DraftModel.findById(team_id).lean();
+      draft = await DraftModel.findById(team_id).lean();
     } else {
-      team = await DraftModel.find({ owner: user_id, leagueId: team_id });
-      team = team[0]
+      draft = await DraftModel.find({ owner: user_id, leagueId: team_id });
+      draft = draft[0]
     }
-    if (team == null) {
+    if (draft == null) {
       return res.status(400).json({ message: 'Team id not found' })
     }
-    res.team = team.toObject();
-    for (let pokemon of res.team) {
+    res.draft = draft;
+    console.log(res.draft)
+    for (let pokemon of res.draft.team) {
       pokemon.name = PokedexService.getName(pokemon.pid)
     }
   } catch (error) {
