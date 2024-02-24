a466b7534aed9f1acc9ffe551c2ca529e8c39df2
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-02-24 01:33:46 -0800

    enable cors

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 //update with pokemondraftzone.com
 
-// app.use(cors());
+app.use(cors());
 
 //app.use('/users', usersRouter);
 //app.use('/teams', leagueRouter);
