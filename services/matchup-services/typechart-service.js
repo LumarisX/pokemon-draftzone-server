990fe0755783b6549eb45d055454cc0489d34b23
LumarisX <Xlumarisx@gmail.com>
2024-02-27 09:16:43 -0800

    add tera types to typechart

--- services/matchup-services/typechart-service.js
+++ services/matchup-services/typechart-service.js
@@ -2,10 +2,18 @@ const PokedexService = require('../pokedex-service.js')
 const TypeService = require('../type-service.js')
 
 function typechart(team) {
+  let teraTypes = {}
   for (let m of team) {
     m.weak = PokedexService.getWeak(m.pid)
+    if ("capt" in m && "tera" in m.capt) {
+      for (let type of m.capt.tera) {
+        if (!(type in teraTypes)) {
+          teraTypes[type] = (TypeService.defensive([type]))
+        }
+      }
+    }
   }
-  tc = {"team": team}
+  tc = { "team": team, "teraTypes": teraTypes }
   return tc
 }
 
