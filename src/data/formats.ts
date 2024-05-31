65b1d25762c1df6484d5a70aa3ba4704732ca302
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-05-31 14:34:06 -0700

    Adding singles level 50 as a format

--- src/data/formats.ts
+++ src/data/formats.ts
@@ -9,6 +9,7 @@ type Formats = {
 
 export const Formats = {
   Singles: { level: 100, choose: 6 },
+  "Singles 50": { level: 50, choose: 6 },
   VGC: { level: 50, choose: 4 },
   Doubles: { level: 100, choose: 6 },
   LC: { level: 5, choose: 6 },
