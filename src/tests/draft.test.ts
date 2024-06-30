015e84733b933a830f06f50fccb91ce0f6581861
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-06-29 19:10:41 -0700

    Delete src/tests directory

--- src/tests/draft.test.ts
+++
@@ -1,6 +0,0 @@
-import { Rulesets } from "../data/rulesets";
-import { getStats } from "../services/database-services/draft.services";
-
-export async function statsTest() {
-  return await getStats(Rulesets["Gen9 NatDex"], "65ecb9da2f6bf04ab58146af");
-}
