a63a121c29c411829da1cd3f3d9d424f4bd42484
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-05-20 13:22:02 -0700

    fix randomizer logic

--- src/services/data-services/pokedex.service.ts
+++ src/services/data-services/pokedex.service.ts
@@ -1,4 +1,4 @@
-import { ID, SpeciesName, TypeName } from "@pkmn/data";
+import { Specie, SpeciesName, TypeName } from "@pkmn/data";
 import { Format } from "../../data/formats";
 import { getRuleset, Ruleset } from "../../data/rulesets";
 
@@ -28,31 +28,38 @@ export function getRandom(
     ? "doublesTier"
     : "tier";
 
-  const eligibleSpecies = Array.from(ruleset.species).filter((pokemon) => {
-    if (!pokemon.exists) return false;
-    if (options.nfe && pokemon.nfe) return false;
-    if (pokemon.forme) return false;
-    if (
-      options.types &&
-      options.types.some((type) => pokemon.types.includes(type))
-    )
-      return false;
-    if (
-      options.tier &&
-      !(
-        pokemon.tier === options.tier ||
-        pokemon[tierLabel] === `(${options.tier})`
-      )
-    )
-      return false;
-    return true;
-  });
+  const eligibleSpecies = Object.entries(
+    Array.from(ruleset.species)
+      .filter((pokemon) => {
+        if (!pokemon.exists) return false;
+        if (pokemon.battleOnly) return false;
+        if (options.nfe && pokemon.nfe) return false;
+        if (
+          options.types &&
+          options.types.some((type) => pokemon.types.includes(type))
+        )
+          return false;
+        if (
+          options.tier &&
+          !(
+            pokemon.tier === options.tier ||
+            pokemon[tierLabel] === `(${options.tier})`
+          )
+        )
+          return false;
+        return true;
+      })
+      .reduce((formGroups, specie) => {
+        const id = specie.changesFrom
+          ? ruleset.species.get(specie.changesFrom)!.id
+          : specie.id;
+        if (!(id in formGroups)) formGroups[id] = [];
+        formGroups[id].push(specie);
+        return formGroups;
+      }, {} as { [key: string]: Specie[] })
+  );
 
-  if (eligibleSpecies.length === 0) {
-    return [];
-  }
-
-  const selectedPokemon: typeof eligibleSpecies = [];
+  const selectedPokemon: Specie[] = [];
   const bannedIds = new Set<string>(options.banned);
   const availableSpecies = [...eligibleSpecies];
 
@@ -61,9 +68,10 @@ export function getRandom(
     availableSpecies.length > 0
   ) {
     const randIndex = Math.floor(Math.random() * availableSpecies.length);
-    const specie = availableSpecies[randIndex];
-    if (!bannedIds.has(specie.id)) {
-      selectedPokemon.push(specie);
+    const specieGroup = availableSpecies[randIndex];
+    if (!bannedIds.has(specieGroup[0])) {
+      const randFormIndex = Math.floor(Math.random() * specieGroup[1].length);
+      selectedPokemon.push(specieGroup[1][randFormIndex]);
       availableSpecies.splice(randIndex, 1);
     } else {
       availableSpecies.splice(randIndex, 1);
