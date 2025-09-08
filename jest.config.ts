3219a3e75929d55fbcd83b5f0aab8742fd5c316e
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-09-08 08:59:10 -0700

    add draft.spec.ts unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -67,11 +67,7 @@ const config: Config = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    "ts-jest": {
-      useBabelrc: true, // Allow Babel settings for TypeScript
-    },
-  },
+  globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -180,7 +176,13 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }], // Add your tsconfig path
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+        babelConfig: true,
+      },
+    ],
   },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
