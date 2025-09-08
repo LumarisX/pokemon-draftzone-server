aa073134d10caafd104c8aed95a5515346e225d2
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-09-08 16:44:20 -0700

    add inspector

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,9 @@ module.exports = {
       script: "npm",
       args: "start",
 
-      node_args: "--max-old-space-size=1536",
+      node_args: `--max-old-space-size=1536 --inspect=0.0.0.0:${
+        9229 + (parseInt(process.env.NODE_APP_INSTANCE) || 0)
+      }`,
 
       max_memory_restart: "1536M",
     },
