a3d6155defa4c0b57021b629db19618c24746324
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-09-08 17:10:57 -0700

    try passing to child object

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,11 @@ module.exports = {
       script: "npm",
       args: "start",
 
-      node_args: `--max-old-space-size=1536 --inspect=0.0.0.0:${
-        9229 + (parseInt(process.env.NODE_APP_INSTANCE) || 0)
-      }`,
+      node_args: `--max-old-space-size=1536`,
+      
+      env: {
+        NODE_OPTIONS: `--inspect=0.0.0.0:${9229 + (parseInt(process.env.NODE_APP_INSTANCE) || 0)}`
+      },
 
       max_memory_restart: "1536M",
     },
