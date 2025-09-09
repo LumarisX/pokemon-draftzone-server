8ef8bc3de6db6caff6ce12bf1bf982443e1c17c4
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-09-08 17:18:16 -0700

    change ecosystem path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,11 @@ module.exports = {
     {
       name: "pmdz",
 
-      script: "npm",
-      args: "start",
+      script: "./dist/index.js",
 
-      node_args: `--max-old-space-size=1536`,
-      
-      env: {
-        NODE_OPTIONS: `--inspect=0.0.0.0:${9229 + (parseInt(process.env.NODE_APP_INSTANCE) || 0)}`
-      },
+      node_args: `--max-old-space-size=1536 --inspect=0.0.0.0:${
+        9229 + (parseInt(process.env.NODE_APP_INSTANCE) || 0)
+      }`,
 
       max_memory_restart: "1536M",
     },
