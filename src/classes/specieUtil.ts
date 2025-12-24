c9fd879b5b54d568a745943a144147c890cd817f
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-12-24 10:48:56 -0800

    update cst logic

--- src/classes/specieUtil.ts
+++ src/classes/specieUtil.ts
@@ -6,10 +6,42 @@ export function getBST(specie: Specie) {
 
 export function getCST(specie: Specie) {
   const baseStats = specie.baseStats;
+
   return Math.round(
-    baseStats.hp +
-      Math.max(baseStats.atk, baseStats.spa) +
-      (baseStats.spa + baseStats.spd) / 2 +
-      baseStats.spe
+    competativeHP(baseStats.hp) +
+      competitiveAttacks(baseStats.atk, baseStats.spa) +
+      competitiveDefenses(baseStats.def, baseStats.spd) +
+      competitiveSpeed(baseStats.spe)
   );
+}
+
+export function competativeHP(hp: number): number {
+  return hp;
+}
+
+export function competitiveAttacks(atk: number, spa: number): number {
+  return parabolicStat(atk, spa, 128);
+}
+
+export function competitiveDefenses(def: number, spd: number): number {
+  return parabolicStat(def, spd, -128);
+}
+
+export function competitiveSpeed(spe: number): number {
+  return sigmoidStat(spe, 80, 256);
+}
+
+function parabolicStat(x: number, y: number, amplitude: number): number {
+  return x + y + Math.pow(x - y, 2) / (2 * amplitude);
+}
+
+function squaredAverage(x: number, y: number): number {
+  return Math.sqrt((x * x + y * y) / 2);
+}
+
+function sigmoidStat(x: number, center: number, amplitude: number): number {
+  const h = 1.5 * center - amplitude / 2;
+  const s = -4 / (amplitude - center);
+
+  return (amplitude - center) / (1 + Math.exp(s * (x - center))) + h;
 }
