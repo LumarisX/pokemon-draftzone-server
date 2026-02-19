11b2760e5b717894a42344d092077d410f0f19e7
LumarisX <65790297+LumarisX@users.noreply.github.com>
2026-02-19 10:10:32 -0800

    fix getDrafted

--- src/services/league-services/tier-list-service.ts
+++ src/services/league-services/tier-list-service.ts
@@ -1,21 +1,21 @@
+import { ID } from "@pkmn/data";
 import { Types } from "mongoose";
-import LeagueTournamentModel, {
-  LeagueTournament,
-  LeagueTournamentDocument,
-} from "../../models/league/tournament.model";
+import { DraftSpecie } from "../../classes/pokemon";
+import { getRuleset } from "../../data/rulesets";
+import { ErrorCodes } from "../../errors/error-codes";
+import { PDZError } from "../../errors/pdz-error";
+import LeagueDivisionModel from "../../models/league/division.model";
+import { LeagueTeamDocument } from "../../models/league/team.model";
 import tierListModel, {
   LeagueTier,
   LeagueTierListDocument,
   LeagueTierListPokemon,
   TierListPokemonAddon,
 } from "../../models/league/tier-list.model";
-import { ID } from "@pkmn/data";
-import { DraftSpecie } from "../../classes/pokemon";
-import { getRuleset } from "../../data/rulesets";
-import { ErrorCodes } from "../../errors/error-codes";
-import { PDZError } from "../../errors/pdz-error";
-import { LeagueDivisionDocument } from "../../models/league/division.model";
-import { LeagueTeamDocument } from "../../models/league/team.model";
+import LeagueTournamentModel, {
+  LeagueTournament,
+  LeagueTournamentDocument,
+} from "../../models/league/tournament.model";
 
 export async function getPokemonTier(
   tournamentId: Types.ObjectId | LeagueTournamentDocument,
@@ -50,11 +50,11 @@ export async function getPokemonTier(
 
 const UNTIERED_TIER_NAME = "Untiered";
 export async function getTierList(
-  league: LeagueTournament,
+  tournament: LeagueTournament,
   showAll: boolean = false,
 ) {
   try {
-    const tierList = await tierListModel.findById(league.tierList);
+    const tierList = await tierListModel.findById(tournament.tierList);
     if (!tierList) throw new PDZError(ErrorCodes.TIER_LIST.NOT_FOUND);
 
     const ruleset = getRuleset(tierList.ruleset);
@@ -146,55 +146,51 @@ export async function getTierList(
 }
 
 export async function getDrafted(
-  league: LeagueTournamentDocument,
+  tournament: LeagueTournamentDocument,
   divisionNames?: string | string[],
 ): Promise<{
   [key: string]: { pokemonId: string }[];
 }> {
-  await league.populate([
-    {
-      path: "divisions",
-      populate: {
-        path: "teams",
-      },
+  const divisionFilter: string[] | undefined = divisionNames
+    ? Array.isArray(divisionNames)
+      ? divisionNames
+      : [divisionNames]
+    : undefined;
+
+  const divisions = await LeagueDivisionModel.find({
+    tournament: tournament._id,
+    ...(divisionFilter && { divisionKey: { $in: divisionFilter } }),
+  }).populate({
+    path: "teams",
+  });
+
+  return divisions.reduce(
+    (acc, division) => {
+      acc[division.name] = (division.teams as LeagueTeamDocument[]).reduce(
+        (teamAcc, team) => {
+          const teamDrafts = team.draft.map((draft) => ({
+            pokemonId: draft.pokemon.id,
+          }));
+          return [...teamAcc, ...teamDrafts];
+        },
+        [] as { pokemonId: string }[],
+      );
+
+      return acc;
     },
-  ]);
-
-  const divisionNameSet = divisionNames
-    ? new Set(Array.isArray(divisionNames) ? divisionNames : [divisionNames])
-    : null;
-
-  return (league.divisions as LeagueDivisionDocument[])
-    .filter((division) =>
-      divisionNameSet ? divisionNameSet.has(division.divisionKey) : true,
-    )
-    .reduce(
-      (acc, division) => {
-        acc[division.name] = (division.teams as LeagueTeamDocument[]).reduce(
-          (teamAcc, team) => {
-            const teamDrafts = team.draft.map((draft) => ({
-              pokemonId: draft.pokemon.id,
-            }));
-            return [...teamAcc, ...teamDrafts];
-          },
-          [] as { pokemonId: string }[],
-        );
-
-        return acc;
-      },
-      {} as { [key: string]: { pokemonId: string }[] },
-    );
+    {} as { [key: string]: { pokemonId: string }[] },
+  );
 }
 
 export async function updateTierList(
-  league: LeagueTournament,
+  tournament: LeagueTournament,
   clientTiers: {
     name: string;
     cost: number;
     pokemon: { id: string; name: string }[];
   }[],
 ) {
-  const tierList = await tierListModel.findById(league.tierList);
+  const tierList = await tierListModel.findById(tournament.tierList);
   if (!tierList) throw new PDZError(ErrorCodes.TIER_LIST.NOT_FOUND);
 
   const validTiers = clientTiers.filter(
