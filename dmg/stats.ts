d10acf559c71ce8e070b52e64f78899521335873
LumarisX <65790297+LumarisX@users.noreply.github.com>
2026-03-17 12:49:56 -0700

    update dmg to use local math file

--- dmg/stats.ts
+++ dmg/stats.ts
@@ -1,15 +1,23 @@
-import type {Generation, StatsTable} from '@pkmn/data';
+import type { Generation, StatsTable } from "@pkmn/data";
 
-import {State} from '../../pokemon-draftzone-server/dmg/state';
-import {abs, clamp, floor, trunc} from '../../pokemon-draftzone-server/dmg/math';
+import { State } from "./state";
+import { abs, clamp, floor, trunc } from "./math";
 
-const LEGACY_BOOSTS = [25, 28, 33, 40, 50, 66, 100, 150, 200, 250, 300, 350, 400];
+const LEGACY_BOOSTS = [
+  25, 28, 33, 40, 50, 66, 100, 150, 200, 250, 300, 350, 400,
+];
 
-export function computeBoostedStat(stat: number, mod: number, gen?: Generation) {
+export function computeBoostedStat(
+  stat: number,
+  mod: number,
+  gen?: Generation,
+) {
   if (gen && gen.num <= 2) {
     return clamp(1, (stat * LEGACY_BOOSTS[mod + 6]) / 100, 999);
   }
-  return floor(trunc(stat * (mod >= 0 ? 2 + mod : 2), 16) / (mod >= 0 ? 2 : abs(mod) + 2));
+  return floor(
+    trunc(stat * (mod >= 0 ? 2 + mod : 2), 16) / (mod >= 0 ? 2 : abs(mod) + 2),
+  );
 }
 
 /**
@@ -19,7 +27,14 @@ export function computeStats(gen: Generation, pokemon: State.Pokemon) {
   const stats = {} as StatsTable;
   if (pokemon.stats) {
     for (const stat of gen.stats) {
-      stats[stat] = stat === 'hp' ? pokemon.stats[stat] : computeBoostedStat(pokemon.stats[stat], pokemon.boosts?.[stat] || 0, gen);
+      stats[stat] =
+        stat === "hp"
+          ? pokemon.stats[stat]
+          : computeBoostedStat(
+              pokemon.stats[stat],
+              pokemon.boosts?.[stat] || 0,
+              gen,
+            );
     }
     return stats;
   } else {
@@ -30,10 +45,14 @@ export function computeStats(gen: Generation, pokemon: State.Pokemon) {
         pokemon.ivs?.[stat] ?? 31,
         pokemon.evs?.[stat] ?? (gen.num <= 2 ? 252 : 0),
         pokemon.level,
-        gen.natures.get(pokemon.nature!)
+        gen.natures.get(pokemon.nature!),
       );
-      if (stat !== 'hp') {
-        stats[stat] = computeBoostedStat(stats[stat], pokemon.boosts?.[stat] || 0, gen);
+      if (stat !== "hp") {
+        stats[stat] = computeBoostedStat(
+          stats[stat],
+          pokemon.boosts?.[stat] || 0,
+          gen,
+        );
       }
     }
   }
